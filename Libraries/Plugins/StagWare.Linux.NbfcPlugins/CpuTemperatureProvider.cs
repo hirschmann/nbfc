6ca8890190036a0f3599400b144c161e528c0ac2
Stefan Hirschmann <st.hirschmann@t-online.de>
2014-08-28 23:07:12 +0200

    [new] Created linux plugins.

---
+++ Libraries/Plugins/StagWare.Linux.NbfcPlugins/CpuTemperatureProvider.cs
@@ -0,0 +1,51 @@
+﻿using StagWare.FanControl.Plugins;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StagWare.Linux.NbfcPlugins
+{
+    [Export(typeof(ITemperatureProvider))]
+    [FanControlPluginMetadata("StagWare.Linux.CpuTempProvider", PlatformID.Unix, MinOSVersion = "13.0")]
+    public class CpuTemperatureProvider : ITemperatureProvider
+    {
+        const string cpuTempFilePath = "/sys/class/hwmon/hwmon0/cpu1_input";
+
+        public bool IsInitialized
+        {
+            get;
+            private set;
+        }
+
+        public void Initialize()
+        {
+            this.IsInitialized = true;
+        }
+
+        public double GetTemperature()
+        {
+            double temp = 0;
+
+            if (double.TryParse(File.ReadAllText(cpuTempFilePath), out temp))
+            {
+                Debug.WriteLine("GetTemperature failed");
+            }
+            else
+            {
+                Debug.WriteLine(string.Format("GetTemperature succeeded: {0.00}", temp));
+            }
+
+            return temp;
+        }
+
+        public void Dispose()
+        {
+            Debug.WriteLine("Disposed CpuTemperatureProvider");
+        }
+    }
+}
---
+++ Libraries/Plugins/StagWare.Linux.NbfcPlugins/EmbeddedController.cs
@@ -0,0 +1,65 @@
+﻿using StagWare.FanControl.Plugins;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StagWare.Linux.NbfcPlugins
+{
+    [Export(typeof(IEmbeddedController))]
+    [FanControlPluginMetadata("StagWare.Linux.EmbeddedController", PlatformID.Unix, MinOSVersion = "13.0")]
+    public class EmbeddedController : IEmbeddedController
+    {
+        public bool IsInitialized
+        {
+            get;
+            private set;
+        }
+
+        public void Initialize()
+        {
+            this.IsInitialized = true;
+        }
+
+        public void WriteByte(byte register, byte value)
+        {
+            Debug.WriteLine(string.Format("WriteByte: {0} => {1}", value, register));
+        }
+
+        public void WriteWord(byte register, ushort value)
+        {
+            Debug.WriteLine(string.Format("WriteWord: {0} => {1}", value, register));
+        }
+
+        public byte ReadByte(byte register)
+        {
+            Debug.WriteLine(string.Format("ReadByte: {0}", register));
+            return 0;
+        }
+
+        public ushort ReadWord(byte register)
+        {
+            Debug.WriteLine(string.Format("ReadWord: {0}", register));
+            return 0;
+        }
+
+        public bool AquireLock(int timeout)
+        {
+            Debug.WriteLine("AquireLock");
+            return true;
+        }
+
+        public void ReleaseLock()
+        {
+            Debug.WriteLine("ReleaseLock");
+        }
+
+        public void Dispose()
+        {
+            Debug.WriteLine("Dispose EmbeddedController");
+        }
+    }
+}
