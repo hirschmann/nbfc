79c40b56eec30f59abbffa1d7de5bc825bea0019
Stefan Hirschmann <st.hirschmann@t-online.de>
2014-08-28 13:23:34 +0200

    [chg] Refactored xml (setup) and xaml (client).

--- Windows/Setup/DriverSetupWixAction/CustomAction.cs
+++ Windows/Setup/DriverSetupWixAction/CustomAction.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Deployment.WindowsInstaller;
+﻿using Microsoft.Deployment.WindowsInstaller;
 using OpenHardwareMonitor.Hardware;
+using System;
 using System.IO;
 
 namespace DriverSetupWixAction
