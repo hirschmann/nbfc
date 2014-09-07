78764b1f652976f13bb382a2252cd77a5f1829bf
Stefan Hirschmann <st.hirschmann@t-online.de>
2014-09-07 15:18:44 +0200

    [chg] Removed Command Line Parser Library

---
+++ Libraries/NbfcCli/Parser.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace NbfcCli
+{
+    public static class Parser
+    {
+        delegate Tuple<bool, T> TryParseAction<T>(string s);
+
+        public static bool TryGetOption(string[] args, string[] aliases, out int value)
+        {
+            TryParseAction<int> action =
+                s =>
+                {
+                    int v;
+                    bool b = int.TryParse(s, NumberStyles.Any, CultureInfo.InvariantCulture, out v);
+                    return new Tuple<bool, int>(b, v);
+                };
+
+            return TryGetOption(action, args, aliases, out value);
+        }
+
+        public static bool TryGetOption(string[] args, string[] aliases, out string value)
+        {
+            TryParseAction<string> action =
+                s =>
+                {
+                    return new Tuple<bool, string>(true, s);
+                };
+
+            return TryGetOption(action, args, aliases, out value);
+        }
+
+        public static bool TryGetOption(string[] args, string[] aliases, out float value)
+        {
+            TryParseAction<float> action =
+                s =>
+                {
+                    float v;
+                    bool b = float.TryParse(s, NumberStyles.Any, CultureInfo.InvariantCulture, out v);
+                    return new Tuple<bool, float>(b, v);
+                };
+
+            return TryGetOption(action, args, aliases, out value);
+        }
+
+        private static bool TryGetOption<T>(TryParseAction<T> action, string[] args, string[] aliases, out T value)
+        {
+            for (int i = 0; i < args.Length - 1; i++)
+            {
+                string s = args[i].Trim();
+
+                if (aliases.Any(x => x.Equals(s, StringComparison.OrdinalIgnoreCase)))
+                {
+                    var result = action(args[i + 1]);
+                    value = result.Item2;
+
+                    return result.Item1;
+                }
+            }
+
+            value = default(T);
+            return false;
+        }
+    }
+}
--- Libraries/NbfcCli/Program.cs
+++ Libraries/NbfcCli/Program.cs
@@ -1,119 +1,240 @@
-﻿using CommandLine;
-using NbfcCli.NbfcService;
+﻿using NbfcCli.NbfcService;
 using System;
 using System.Globalization;
 using System.Text;
 
 namespace NbfcCli
 {
-    class Program
+    public class Program
     {
+        #region Constants
+
+        const string VerbStart = "start";
+        const string VerbStop = "stop";
+        const string VerbStatus = "status";
+        const string VerbLoad = "load";
+        const string VerbSet = "set";
+
+        static readonly string[] LoadConfigOptions = { "-c", "--config" };
+        static readonly string[] SetSpeedOptions = { "-s", "--speed" };
+        static readonly string[] SetIndexOptions = { "-i", "--index" };
+
+        #endregion
+
+        #region Main
+
         static void Main(string[] args)
         {
-            var options = new CommandLineOptions();
-
-            var onVerb = new Action<string, object>((s, o) =>
+            try
             {
-                switch (s.ToLower())
-                {
-                    case "start":
-                        options.Start = true;
-                        break;
-
-                    case "stop":
-                        options.Stop = true;
-                        break;
-
-                    case "status":
-                        options.StatusVerb = (StatusVerbSubOptions)o;
-                        break;
-
-                    case "load":
-                        options.LoadVerb = (LoadVerbSubOptions)o;
-                        break;
-
-                    case "set":
-                        options.SetVerb = (SetVerbSubOptions)o;
-                        break;
-                }
-            });
-
-            var settings = new ParserSettings();
-            settings.CaseSensitive = false;
-            settings.IgnoreUnknownArguments = false;
-            settings.MutuallyExclusive = true;
-            settings.ParsingCulture = CultureInfo.InvariantCulture;
-            settings.HelpWriter = Console.Error;
-
-
-            using (Parser parser = new Parser(settings))
+                ParseArgs(args);
+            }
+            catch (Exception e)
             {
-                if (parser.ParseArgumentsStrict(args, options, onVerb))
+                Console.Error.WriteLine(e.Message);
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private static void ParseArgs(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                switch (args[0].ToLower())
                 {
-                    using (var client = new FanControlServiceClient())
-                    {
-                        client.Open();
+                    case VerbStart:
+                        if (args.Length == 1)
+                        {
+                            StartService();
+                        }
+                        else
+                        {
+                            Console.Error.WriteLine("Invalid number of arguments");
+                        }
+                        break;
 
-                        if (options.Start)
+                    case VerbStop:
+                        if (args.Length == 1)
                         {
-                            client.Start();
+                            StopService();
                         }
-                        else if (options.Stop)
+                        else
                         {
-                            client.Stop();
+                            Console.Error.WriteLine("Invalid number of arguments");
                         }
-                        else if (options.LoadVerb != null)
-                        {
-                            client.SetConfig(options.LoadVerb.ConfigName);
-                        }
-                        else if (options.SetVerb != null)
-                        {
-                            client.SetTargetFanSpeed(
-                                options.SetVerb.Speed,
-                                options.SetVerb.FanIndex);
-                        }
-                        else if (options.StatusVerb != null)
-                        {
-                            var info = client.GetFanControlInfo();
+                        break;
 
-                            var sb = new StringBuilder();
-                            sb.AppendFormat("Service enabled\t\t: {0}", info.Enabled);
-                            sb.AppendLine();
-                            sb.AppendFormat("Selected config name\t: {0}", info.SelectedConfig);
-                            sb.AppendLine();
-                            sb.AppendFormat("Temperature\t\t: {0}", info.CpuTemperature);
-                            sb.AppendLine();
+                    case VerbStatus:
+                        if (args.Length == 1)
+                        {
+                            PrintStatus();
+                        }
+                        else
+                        {
+                            Console.Error.WriteLine("Invalid number of arguments");
+                        }
+                        break;
 
-                            if (options.StatusVerb.FanIndex >= 0
-                                && options.StatusVerb.FanIndex < info.FanStatus.Length)
+                    case VerbLoad:
+                        if (args.Length == 3)
+                        {
+                            string s;
+
+                            if (Parser.TryGetOption(args, LoadConfigOptions, out s))
                             {
-                                sb.AppendLine();
-                                sb.Append(FanStatusToString(info.FanStatus[options.StatusVerb.FanIndex]));
-                            }
-                            else if (options.StatusVerb.GetAll)
-                            {
-                                foreach (FanStatus status in info.FanStatus)
-                                {
-                                    sb.AppendLine();
-                                    sb.Append(FanStatusToString(status));
-                                }
+                                LoadConfig(s);
                             }
                             else
                             {
-                                Console.WriteLine(options.GetHelp());
+                                Console.Error.WriteLine("Arguments could not be parsed");
                             }
-
-                            Console.Write(sb.ToString());
+                        }
+                        else
+                        {
+                            Console.Error.WriteLine("Invalid number of arguments");
                         }
 
-                        client.Close();
-                    }
-                }
-                else
-                {
-                    Console.WriteLine(options.GetHelp());
+                        break;
+
+                    case VerbSet:
+                        if (args.Length == 3 || args.Length == 5)
+                        {
+                            float speed;
+                            int index = 0;
+
+                            if (Parser.TryGetOption(args, SetSpeedOptions, out speed))
+                            {
+                                Parser.TryGetOption(args, SetIndexOptions, out index);
+                                SetFanSpeed(speed, index);
+                            }
+                            else
+                            {
+                                Console.Error.WriteLine("Arguments could not be parsed");
+                            }
+                        }
+                        else
+                        {
+                            Console.Error.WriteLine("Invalid number of arguments");
+                        }
+
+                        break;
+
+                    default:
+                        PrintHelp();
+                        break;
                 }
             }
+            else
+            {
+                PrintHelp();
+            }
+        }
+
+        private static void SetFanSpeed(float speed, int index)
+        {
+            using (var client = new FanControlServiceClient())
+            {
+                client.Open();
+                client.SetTargetFanSpeed(speed, index);
+                client.Close();
+            }
+        }
+
+        private static void LoadConfig(string configName)
+        {
+            using (var client = new FanControlServiceClient())
+            {
+                client.Open();
+                client.SetConfig(configName);
+                client.Close();
+            }
+        }
+
+        private static void PrintStatus()
+        {
+            using (var client = new FanControlServiceClient())
+            {
+                client.Open();
+
+                var info = client.GetFanControlInfo();
+
+                var sb = new StringBuilder();
+                sb.AppendFormat("Service enabled\t\t: {0}", info.Enabled);
+                sb.AppendLine();
+                sb.AppendFormat("Selected config name\t: {0}", info.SelectedConfig);
+                sb.AppendLine();
+                sb.AppendFormat("Temperature\t\t: {0}", info.CpuTemperature);
+                sb.AppendLine();
+
+                foreach (FanStatus status in info.FanStatus)
+                {
+                    sb.AppendLine();
+                    sb.Append(FanStatusToString(status));
+                }
+
+                Console.WriteLine(sb.ToString());
+                client.Close();
+            }
+        }
+
+        private static void StartService()
+        {
+            using (var client = new FanControlServiceClient())
+            {
+                client.Open();
+                client.Start();
+                client.Close();
+            }
+        }
+
+        private static void StopService()
+        {
+            using (var client = new FanControlServiceClient())
+            {
+                client.Open();
+                client.Stop();
+                client.Close();
+            }
+        }
+
+        private static void PrintHelp()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("NoteBook FanControl command line client");
+            sb.AppendLine();
+            sb.AppendLine("Commands:");
+            sb.AppendFormat("  {0,-10}Start the fan control service", VerbStart);
+            sb.AppendLine();
+            sb.AppendFormat("  {0,-10}Stop the fan control service", VerbStop);
+            sb.AppendLine();
+            sb.AppendFormat("  {0,-10}Show the fan control status", VerbStatus);
+            sb.AppendLine();
+            sb.AppendLine();            
+            sb.AppendFormat("  {0,-10}{1} [{2}]", VerbSet, string.Join(" | ", SetSpeedOptions), string.Join(" | ", SetIndexOptions));
+            sb.AppendLine();
+            sb.AppendFormat("  {0,-10}Set the speed for a single fan", "");
+            sb.AppendLine();
+            sb.AppendFormat("   {0,-9}Fan speed in percent", SetSpeedOptions[0]);
+            sb.AppendLine();
+            sb.AppendFormat("   {0,-9}Fan index (default = 0)", SetIndexOptions[0]);
+            sb.AppendLine();
+            sb.AppendLine();            
+            sb.AppendFormat("  {0,-10}{1}", VerbLoad, string.Join(" | ", LoadConfigOptions));
+            sb.AppendLine();
+            sb.AppendFormat("  {0,-10}Load and apply a config", "");
+            sb.AppendLine();
+            sb.AppendFormat("   {0,-9}Name of the config", LoadConfigOptions[0]);
+            sb.AppendLine();
+            sb.AppendLine();
+            sb.AppendLine("Examples:");
+            sb.AppendLine("  nbfc start");
+            sb.AppendLine("  nfbc load -c 'HP ProBook 6465b'");
+            sb.AppendLine("  nbfc set -s 12.3");
+
+            Console.WriteLine(sb.ToString());
         }
 
         private static string FanStatusToString(FanStatus status)
@@ -133,5 +254,7 @@ namespace NbfcCli
 
             return sb.ToString();
         }
+
+        #endregion
     }
 }
