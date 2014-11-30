3de4318d53075dd4123a57359df4a01f666481e5
Stefan Hirschmann <st.hirschmann@t-online.de>
2014-11-30 20:07:07 +0100

    Updated assembly version.

--- Windows/NbfcClient/Properties/AssemblyInfo.cs
+++ Windows/NbfcClient/Properties/AssemblyInfo.cs
@@ -49,6 +49,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.33")]
-[assembly: AssemblyFileVersion("1.2.0.33")]
+[assembly: AssemblyVersion("1.2.3.35")]
+[assembly: AssemblyFileVersion("1.2.3.35")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
