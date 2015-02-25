d9a8b4ea505cb6b969130e1434d4801f8b7f3008
Stefan Hirschmann <st.hirschmann@t-online.de>
2015-02-25 15:14:42 +0100

    Change target framework

--- Core/Plugins/OpenHardwareMonitor/Properties/AssemblyVersion.cs
+++ Core/Plugins/OpenHardwareMonitor/Properties/AssemblyVersion.cs
@@ -10,5 +10,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.7.1.0")]
-[assembly: AssemblyInformationalVersion("0.7.1 Beta")]
+[assembly: AssemblyVersion("0.7.100.0")]
+[assembly: AssemblyInformationalVersion("0.7.100 Beta NBFC")]
