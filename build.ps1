d5a93f0c5693a21d87c0cb0edf1a6f4686b6d0b8
Stefan Hirschmann <st.hirschmann@t-online.de>
2015-05-06 17:22:38 +0200

    Add comments

--- build.ps1
+++ build.ps1
@@ -1,11 +1,15 @@
 $workingDir = split-path $MyInvocation.MyCommand.Path
 push-location $workingDir
 
+# download nuget if necessary
 if(!(test-path nuget.exe)) {
 	(new-object System.Net.WebClient).DownloadFile('http://nuget.org/nuget.exe', "$workingDir\nuget.exe")
 }
 
+# restore nuget packages for solution
 nuget.exe restore
+
+# build solution
 & $env:windir\Microsoft.NET\Framework\v4.0.*\msbuild.exe /t:Build /p:Configuration=ReleaseWindows NoteBookFanControl.sln
 
 pop-location
