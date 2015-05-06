9c117dfb272a6b812d846aeccf916f4d7954f6d3
Stefan Hirschmann <st.hirschmann@t-online.de>
2015-05-06 17:22:14 +0200

    Restore nuget packages before build

--- build.sh
+++ build.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-xbuild /t:Build /p:Configuration=ReleaseLinux "$(dirname -- "$0")/NoteBookFanControl.sln"
+pushd $(dirname "${0}")
+
+# download nuget if necessary
+if [ -f ./nuget.exe ]
+then
+	wget http://nuget.org/nuget.exe
+fi
+
+# import Mozilla trusted root certificates into mono certificates store
+mozroots --import --sync
+
+# restore nuget packages for solution
+mono nuget.exe restore
+
+# build solution
+xbuild /t:Build /p:Configuration=ReleaseLinux NoteBookFanControl.sln
+
+popd 
