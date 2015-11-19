2ab77e329697aaccd8d0378a27201eb8c2c3e3ae
Stefan Hirschmann <st.hirschmann@t-online.de>
2015-11-19 21:55:18 +0100

    Update assembly info

--- Core/NbfcProbe/Properties/AssemblyInfo.cs
+++ Core/NbfcProbe/Properties/AssemblyInfo.cs
@@ -1,22 +1,28 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
 
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 [assembly: AssemblyTitle("NbfcProbe")]
-[assembly: AssemblyDescription("NBFC Command-line EC-RAM probing tool")]
+[assembly: AssemblyDescription("Command line EC-RAM probing tool.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Alexander Schlarb")]
+[assembly: AssemblyCompany("StagWare")]
+[assembly: AssemblyProduct("NbfcProbe")]
+[assembly: AssemblyCopyright("Copyright © 2015 Alexander Schlarb, Stefan Hirschmann")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.0.0")]
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
 
+[assembly: AssemblyFileVersionAttribute("1.1.0.0")]
+[assembly: ComVisibleAttribute(false)]
+[assembly: NeutralResourcesLanguageAttribute("en")]
+[assembly: GuidAttribute("c7e7cd6a-71f9-4489-8a59-55756d6d6de4")]
