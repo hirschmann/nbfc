b2b61585e54e222ad119b2c73e14ebf7364e042d
Stefan Hirschmann <st.hirschmann@t-online.de>
2016-01-19 00:53:18 +0100

    Add new EC plugin for Linux which uses ec_sys

---
+++ StagWare.Plugins.ECSysLinux/ECSysLinux.cs
@@ -0,0 +1,139 @@
+﻿using Mono.Unix;
+using Mono.Unix.Native;
+using StagWare.FanControl.Plugins;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace StagWare.Plugins.ECSysLinux
+{
+    [Export(typeof(IEmbeddedController))]
+    [FanControlPluginMetadata(
+        "StagWare.Plugins.ECSysLinux",
+        SupportedPlatforms.Unix,
+        SupportedCpuArchitectures.x86 | SupportedCpuArchitectures.x64)]
+    public class ECSysLinux : IEmbeddedController
+    {
+        #region Constants
+
+        private const string EC0IOPath = "/sys/kernel/debug/ec/ec0/io";
+
+        #endregion
+
+        #region Private Fields
+
+        static readonly object syncRoot = new object();
+        private UnixStream stream;
+
+        #endregion
+
+        #region IEmbeddedController implementation
+
+        public bool IsInitialized { get; private set; }
+
+        public void Initialize()
+        {
+            try
+            {
+                Process modprobe = new Process();
+                modprobe.StartInfo.FileName = "modprobe";
+                modprobe.StartInfo.Arguments = "ec_sys write_support=1";
+                modprobe.Start();
+                modprobe.WaitForExit();
+
+                IsInitialized = modprobe.ExitCode == 0 && File.Exists(EC0IOPath);
+            }
+            catch
+            {
+                IsInitialized = false;
+            }
+        }
+
+        public void WriteByte(byte register, byte value)
+        {
+            byte[] buffer = new byte[] { value };
+            this.stream.WriteAtOffset(buffer, 0, buffer.Length, register);
+        }
+
+        public void WriteWord(byte register, ushort value)
+        {
+            // little endian
+            byte msb = (byte)(value >> 8);
+            byte lsb = (byte)value;
+
+            byte[] buffer = new byte[] { lsb, msb };
+            this.stream.WriteAtOffset(buffer, 0, buffer.Length, register);
+        }
+
+        public byte ReadByte(byte register)
+        {
+            byte[] buffer = new byte[1];
+            this.stream.ReadAtOffset(buffer, 0, buffer.Length, register);
+
+            return buffer[0];
+        }
+
+        public ushort ReadWord(byte register)
+        {
+            // little endian
+            byte[] buffer = new byte[2];
+            this.stream.ReadAtOffset(buffer, 0, buffer.Length, register);
+
+            return (ushort)((buffer[1] << 8) | buffer[0]);
+        }
+
+        public bool AcquireLock(int timeout)
+        {
+            bool success = false;
+
+            if (Monitor.TryEnter(syncRoot, timeout))
+            {
+                if (this.stream == null)
+                {
+                    int fd = Syscall.open(EC0IOPath, OpenFlags.O_RDWR | OpenFlags.O_EXCL);
+
+                    if (fd == -1)
+                    {
+                        var e = new Win32Exception(Marshal.GetLastWin32Error());
+                        Debug.WriteLine(string.Format("Error opening {0}: {1}", EC0IOPath, e.Message));
+
+                        Monitor.Exit(syncRoot);
+                    }
+                    else
+                    {
+                        this.stream = new UnixStream(fd);
+                        success = true;
+                    }
+                }
+            }
+
+            return success;
+        }
+
+        public void ReleaseLock()
+        {
+            try
+            {
+                if (this.stream != null)
+                {
+                    this.stream.Dispose();
+                    this.stream = null;
+                }
+            }
+            finally
+            {
+                Monitor.Exit(syncRoot);
+            }
+        }
+
+        public void Dispose()
+        {
+            ReleaseLock();
+        }
+
+        #endregion
+    }
+}
---
+++ StagWare.Plugins.ECSysLinux/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("StagWare.Plugins.ECSysLinux")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("StagWare")]
+[assembly: AssemblyProduct("StagWare.Plugins.ECSysLinux")]
+[assembly: AssemblyCopyright("Copyright ©  2016 Stefan Hirschmann")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("62c829d6-7338-4d44-87fc-00b8d01d8157")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
