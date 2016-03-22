d94512ad560949c2444472b723a756585fe42f6a
Stefan Hirschmann <st.hirschmann@t-online.de>
2016-03-22 23:37:10 +0100

    Reference nuget.exe explicitely

--- build.ps1
+++ build.ps1
@@ -2,12 +2,12 @@ $workingDir = split-path $MyInvocation.MyCommand.Path
 push-location $workingDir
 
 # download nuget if necessary
-if(!(test-path nuget.exe)) {
+if(!(test-path .\nuget.exe)) {
 	(new-object System.Net.WebClient).DownloadFile('http://nuget.org/nuget.exe', "$workingDir\nuget.exe")
 }
 
 # restore nuget packages for solution
-nuget.exe restore
+.\nuget.exe restore
 
 # build solution
 & $env:windir\Microsoft.NET\Framework\v4.0.*\msbuild.exe /t:Build /p:Configuration=ReleaseWindows NoteBookFanControl.sln
