d4047ee2649a0d12f4c103e432a3b1bddec1ab3b
Stefan Hirschmann <st.hirschmann@t-online.de>
2016-10-12 15:49:04 +0200

    Update version

--- Core/Plugins/OpenHardwareMonitor/Properties/AssemblyVersion.cs
+++ Core/Plugins/OpenHardwareMonitor/Properties/AssemblyVersion.cs
@@ -10,5 +10,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.7.150.0")]
-[assembly: AssemblyInformationalVersion("0.7.150 Alpha NBFC")]
+[assembly: AssemblyVersion("0.7.160.0")]
+[assembly: AssemblyInformationalVersion("0.7.160 NBFC")]
