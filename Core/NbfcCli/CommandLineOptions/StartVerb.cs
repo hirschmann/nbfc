da758edf4595172d45b78f8e0554b66de1800c5b
Stefan Hirschmann <st.hirschmann@t-online.de>
2016-11-21 09:24:46 +0100

    Add start --enabled option

--- Core/NbfcCli/CommandLineOptions/StartVerb.cs
+++ Core/NbfcCli/CommandLineOptions/StartVerb.cs
@@ -4,11 +4,20 @@ namespace NbfcCli.CommandLineOptions
 {
     public class StartVerb
     {
+        [MutuallyExclusiveGroup("option")]
         [NamedArgumentEx(
             'r',
             "readonly",
             Action = ParseAction.StoreTrue,
             Description = "Start in read-only mode")]
         public bool ReadOnly { get; set; }
+
+        [MutuallyExclusiveGroup("option")]
+        [NamedArgumentEx(
+            'e',
+            "enabled",
+            Action = ParseAction.StoreTrue,
+            Description = "Start in enabled mode")]
+        public bool Enabled { get; set; }
     }
 }
