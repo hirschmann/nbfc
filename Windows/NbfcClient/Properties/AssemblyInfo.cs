8edab5ccf5ac8ab8d7eccac88a0f5f4c6900673f
Stefan Hirschmann <st.hirschmann@t-online.de>
2016-11-22 22:21:55 +0100

    Bump version

--- Windows/NbfcClient/Properties/AssemblyInfo.cs
+++ Windows/NbfcClient/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.4.1.0")]
 [assembly: AssemblyFileVersion("1.4.1.0")]
-[assembly: AssemblyInformationalVersion("1.4.3")]
+[assembly: AssemblyInformationalVersion("1.4.4")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
