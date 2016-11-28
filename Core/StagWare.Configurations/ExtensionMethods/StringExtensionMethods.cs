629debbe08a140cacac6a345108e0c506aae6bd4
Stefan Hirschmann <st.hirschmann@t-online.de>
2016-11-28 13:01:33 +0100

    Add RecommendConfigs() method

---
+++ Core/StagWare.Configurations/ExtensionMethods/StringExtensionMethods.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StagWare.ExtensionMethods
+{
+    public static class StringExtensionMethods
+    {
+        public static string GetLongestCommonSubstring(this string self, string str)
+        {
+            List<string> list = GetLongestCommonSubstrings(self, str);
+
+            if(list.Count > 0)
+            {
+                return list[0];
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+
+        public static List<string> GetLongestCommonSubstrings(this string self, string str)
+        {
+            if(str == null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            string minString, maxString;
+
+            if(self.Length > str.Length)
+            {
+                maxString = self;
+                minString = str;
+            }
+            else
+            {
+                maxString = str;
+                minString = self;
+            }
+
+            var lookup = new int[2][] { new int[minString.Length], new int[minString.Length] };
+            var substrEndIndices = new List<int>();
+            int substrLength = 0;
+
+            for(int i = 0; i < maxString.Length; i++)
+            {
+                int rowIdx = i % 2;
+
+                for(int j = 0; j < minString.Length; j++)
+                {
+                    if(maxString[i] == minString[j])
+                    {
+                        if ((i == 0) || (j == 0))
+                        {
+                            lookup[rowIdx][j] = 1;
+                        }
+                        else
+                        {
+                            lookup[rowIdx][j] = lookup[(rowIdx + 1) % 2][j - 1] + 1;
+                        }
+
+                        if(lookup[rowIdx][j] > substrLength)
+                        {
+                            substrLength = lookup[rowIdx][j];
+                            substrEndIndices.Clear();
+                            substrEndIndices.Add(i);
+                        }
+                        else if(lookup[rowIdx][j] == substrLength)
+                        {
+                            substrEndIndices.Add(i);
+                        }
+                    }
+                    else
+                    {
+                        lookup[rowIdx][j] = 0;
+                    }
+                }
+            }
+
+            return substrEndIndices
+                .Select(x => maxString.Substring((x - substrLength) + 1, substrLength))
+                .ToList();
+        }
+    }
+}
--- Core/StagWare.Configurations/FanControlConfigManager.cs
+++ Core/StagWare.Configurations/FanControlConfigManager.cs
@@ -1,13 +1,36 @@
 ﻿using StagWare.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text.RegularExpressions;
+using StagWare.ExtensionMethods;
 
 namespace StagWare.FanControl.Configurations
 {
     public class FanControlConfigManager : ConfigManager<FanControlConfigV2>
     {
+        #region Constants
+
+        private const string RemoveBracketsPattern = @"[\(\[\{].*?[\)\]\}]";
+
+        #endregion
+
+        #region Private Fields
+
+        private readonly ReadOnlyDictionary<string, string> VendorAliases =
+            new ReadOnlyDictionary<string, string>(new Dictionary<string, string>()
+        {
+            { "Hewlett-Packard", "HP" }
+        });
+
+        #endregion
+
         #region Properties
 
         public FanControlConfigV2 SelectedConfig { get; private set; }
         public string SelectedConfigName { get; private set; }
+        public string DeviceModelName { get; private set; }
 
         #endregion
 
@@ -15,12 +38,14 @@ namespace StagWare.FanControl.Configurations
 
         public FanControlConfigManager(string configsDirPath)
             : base(configsDirPath)
-        {  
+        {
+            DeviceModelName = GetDeviceModelName();
         }
 
         public FanControlConfigManager(string configsDirPath, string configFileExtension)
             : base(configsDirPath, configFileExtension)
-        { 
+        {
+            DeviceModelName = GetDeviceModelName();
         }
 
         #endregion
@@ -43,6 +68,123 @@ namespace StagWare.FanControl.Configurations
             return SelectedConfig != null;
         }
 
+        public List<string> RecommendConfigs()
+        {
+            return RecommendConfigs(DeviceModelName);
+        }
+
+        public List<string> RecommendConfigs(string deviceModel)
+        {
+            deviceModel = Regex.Replace(deviceModel, RemoveBracketsPattern, "");
+
+            if (string.IsNullOrWhiteSpace(deviceModel))
+            {
+                return new List<string>();
+            }
+
+            var dict = new Dictionary<string, Tuple<string, double>>();
+
+            foreach (string cfgName in ConfigNames)
+            {
+                string cleanCfgName = Regex.Replace(cfgName, RemoveBracketsPattern, "");
+                double similarity = GetSimilarityIndex(deviceModel, cleanCfgName);
+
+                if (similarity >= 1)
+                {
+                    FanControlConfigV2 cfg = GetConfig(cleanCfgName);
+
+                    if ((cfg.FanConfigurations == null) || (cfg.FanConfigurations.Count == 0))
+                    {
+                        continue;
+                    }
+
+                    string key = "";
+                    cfg.FanConfigurations.ForEach(x => key += $"{x.ReadRegister}{x.WriteRegister}");
+
+                    if (dict.ContainsKey(key))
+                    {
+                        Tuple<string, double> tuple = dict[key];
+
+                        if (tuple.Item2 < similarity)
+                        {
+                            dict[key] = new Tuple<string, double>(cleanCfgName, similarity);
+                        }
+                    }
+                    else
+                    {
+                        dict.Add(key, new Tuple<string, double>(cleanCfgName, similarity));
+                    }
+                }
+            }
+
+            return dict.Values
+                .OrderByDescending(x => x.Item2)
+                .Select(x => x.Item1)
+                .ToList();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private string GetDeviceModelName()
+        {
+            var biosInfo = BiosInfo.BiosInfo.Create();
+            string sysName = biosInfo?.SystemName.Trim();
+
+            if (sysName == null)
+            {
+                return null;
+            }
+
+            sysName = Regex.Replace(sysName, RemoveBracketsPattern, "").Trim();
+            string vendor = biosInfo?.SystemVendor?.Trim();
+
+            if (vendor != null)
+            {
+                if (VendorAliases.ContainsKey(vendor))
+                {
+                    vendor = VendorAliases[vendor];
+                }
+
+                if (!sysName.StartsWith(vendor, StringComparison.OrdinalIgnoreCase))
+                {
+                    sysName = $"{vendor} {sysName}";
+                }
+            }
+
+            return sysName;
+        }
+
+        private static double GetSimilarityIndex(string modelName1, string modelName2)
+        {
+            double result = 0;
+            modelName1 = modelName1.ToLower();
+            modelName2 = modelName2.ToLower();
+
+            foreach (string s1 in modelName1.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                double maxSimilarity = 0;
+
+                foreach (string s2 in modelName2.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    int lcsLength = s1.GetLongestCommonSubstring(s2).Length;
+
+                    if (lcsLength < 2)
+                    {
+                        continue;
+                    }
+
+                    double similarity = (double)lcsLength / Math.Max(s1.Length, s2.Length);
+                    maxSimilarity = Math.Max(maxSimilarity, similarity);
+                }
+
+                result += maxSimilarity;
+            }
+
+            return result;
+        }
+
         #endregion
 
         #region Overrides
@@ -54,7 +196,7 @@ namespace StagWare.FanControl.Configurations
         }
 
         public override void RemoveConfig(string configName)
-        {            
+        {
             base.RemoveConfig(configName);
 
             if (configName == SelectedConfigName)
