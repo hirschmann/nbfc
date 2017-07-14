31d43285c64ae0a4877c5ff1a003a096c8ec8db7
Stefan Hirschmann <st.hirschmann@t-online.de>
2017-07-14 20:50:20 +0200

    Update nuget to latest recommended version (instead of latest available version) if package restore fails.

--- build.ps1
+++ build.ps1
@@ -1,17 +1,34 @@
 $workingDir = split-path $MyInvocation.MyCommand.Path
 $vswhere = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
+$nugeturl = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+
+function download-nuget {
+	$client = New-Object System.Net.WebClient
+	
+	# Set proxy to null to prevent the web client from trying to retrieve
+	# proxy settings from IE (which is incredibly slow)
+	$client.Proxy = $null
+	$client.DownloadFile($nugeturl, "$workingDir\nuget.exe")
+}
+
 push-location $workingDir
 
 # download nuget if necessary
 if(!(test-path .\nuget.exe)) {
-	(new-object System.Net.WebClient).DownloadFile('http://nuget.org/nuget.exe', "$workingDir\nuget.exe")
+	write-output "NuGet could not be found. Downloading latest recommended version."
+	download-nuget
 }
 
-# update nuget
-.\nuget.exe update -self
-
 # restore nuget packages for solution
-.\nuget.exe restore
+& .\nuget.exe restore
+
+# re-download nuget and retry on error
+if($LASTEXITCODE -ne 0) {	
+	write-output "Packages could not be restored. Updating NuGet."
+	remove-item .\nuget.exe
+	download-nuget
+	& .\nuget.exe restore
+}
 
 # get msbuild path
 $path = & $vswhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath
