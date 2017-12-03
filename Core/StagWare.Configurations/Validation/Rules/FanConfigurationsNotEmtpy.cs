5991f2a4a2fa3b0cfde8cd01ec656343eed0624b
Stefan Hirschmann <st.hirschmann@t-online.de>
2017-12-03 23:55:44 +0100

    Add additional config validation rules

---
+++ Core/StagWare.Configurations/Validation/Rules/FanConfigurationsNotEmtpy.cs
@@ -0,0 +1,26 @@
+﻿namespace StagWare.FanControl.Configurations.Validation.Rules
+{
+    public class FanConfigurationsNotEmtpy : IValidationRule<FanControlConfigV2>
+    {
+        public string Description => "At least one fan configuration must exist";
+
+        public Validation Validate(FanControlConfigV2 item)
+        {
+            var v = new Validation()
+            {
+                RuleDescription = this.Description,
+                Result = ValidationResult.Success
+            };
+
+            if (item.FanConfigurations == null || item.FanConfigurations.Count == 0)
+            {
+                v.Result = ValidationResult.Error;
+                return v;
+            }
+            else
+            {
+                return v;
+            }
+        }
+    }
+}
