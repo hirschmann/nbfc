325fea1fdefbf29f7d21b55bcc0bf5c585a3b699
Leon Bartle <leon@bartle.eu>
2021-07-14 15:12:47 +0200

    version

--- Common/CommonAssemblyInfo.cs
+++ Common/CommonAssemblyInfo.cs
@@ -2,6 +2,6 @@
 using System.Resources;
 
 [assembly: AssemblyCompany("StagWare")]
-[assembly: AssemblyCopyright("Copyright © 2012-2019 Stefan Hirschmann")]
+[assembly: AssemblyCopyright("Copyright © 2012-2019 Stefan Hirschmann, 2021 © Leon Bartle")]
 [assembly: AssemblyTrademark("")]
 [assembly: NeutralResourcesLanguage("en")]
